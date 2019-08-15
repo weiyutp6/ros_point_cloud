926909a63d20450d4b1616cc36fc1d131e0be4ab
weiyutp6 <b07505020@ntu.edu.tw>
2019-08-16 04:06:33 +0800

    final project commit

--- src/ros_point_cloud/src/lidar_detect.py
+++ src/ros_point_cloud/src/lidar_detect.py
@@ -6,10 +6,8 @@ from std_msgs.msg import String
 from publish_utils import *
 if __name__ == '__main__':
 	rospy.init_node('talker',anonymous = True)
-	ego_pub = rospy.Publisher('vrx_ego_boat', Marker,queue_size = 10)
 	rospy.Subscriber('/lidar_wamv/points',PointCloud2,get_pointcloud2_xyz)
 	rate = rospy.Rate(10)
 	while not rospy.is_shutdown():
 	        hello = 'hello world ! %s' % rospy.get_time()
-        	publish_ego_boat(ego_pub)
 		rate.sleep()
--- src/ros_point_cloud/src/publish_utils.py
+++ src/ros_point_cloud/src/publish_utils.py
@@ -5,35 +5,24 @@ from sensor_msgs import point_cloud2
 from sensor_msgs.msg import PointCloud2
 from std_msgs.msg import Header
 from std_msgs.msg import String
-from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Point
 FRAME_ID ='base_link'
 
+class obstacles:
+    def __init__ (self, x, y) :
+        self.x = x
+        self.y = y
+ 
+
 def callback(data):
 	rospy.loginfo(rospy.get_caller_id() + 'I heard %s' % data.data)
-def publish_ego_boat(ego_boat_pub):
-	marker = Marker()
-	marker.header.frame_id = FRAME_ID
-	marker.header.stamp = rospy.Time.now()
-
-	marker.id = 0
-	marker.action = Marker.ADD
-	marker.lifetime = rospy.Duration()
-	marker.type = Marker.LINE_STRIP
-
-	marker.color.r = 0.0
-	marker.color.g = 1.0
-	marker.color.b = 0.0
-	marker.color.a = 1.0
-	marker.scale.x = 0.2
-
-	marker.points = []
-	marker.points.append(Point(10,-10,0))
-	marker.points.append(Point(0,0,0))
-	marker.points.append(Point(10,10,0))
-
-
-	ego_boat_pub.publish(marker)
+def publish_final_pos(positions):
+        final_pos = ""
+        for final in positions:
+            final_pos += final
+        obstacle_pub = rospy.Publisher('pre_obstacle',String,queue_size = 10)
+        rospy.loginfo(final_pos)
+        obstacle_pub.publish(final_pos)
 	'''marker_array.markers.append(marker)'''
 def get_pointcloud2_xyz(data):
 	'''
@@ -47,43 +36,52 @@ def get_pointcloud2_xyz(data):
 	gen = point_cloud2.read_points(data, field_names=("x", "y", "z"), skip_nans=True)
 	rate.sleep()
 	print type(gen)
-        obstacle_pub = rospy.Publisher('pre_obstacle',String,queue_size = 10)
         obstacle_x = 0
-        class obstacles:
-            def __init__ (self, x, y) :
-                self.x = x
-                self.y = y
         obstacleslist = []
+        leftlist = []
         obstacle_y = 0
         counter = 0
         first = True
         pos_x = 0
         pox_y = 0
 	for p in gen:
-                if p[0]>0:
-                        if (p[0]*p[0]) < 400 :
-                            if (p[1]*p[1]) < 400:
-                                if first == True :
+                if p[0]>1:
+                    if (p[0]*p[0]) < 400 :
+                         if (p[1]*p[1]) < 400:
+                              if first == True :
                                     pos_x = p[0]
                                     pos_y = p[1]
                                     first = False 
-                                if (p[0]-pos_x)*(p[0]-pos_x)+(p[1]-pos_y)*(p[1]-pos_y)>0.4:
-                                    pos_x = obstacle_x/counter
-                                    pos_y = obstacle_y/counter
-                                    print " obstacle : ( %.3f , %.3f ) " %(pos_x , pos_y)
-                                    obstacle = " ( %.3f , %.3f ) " %(pos_x , pos_y)
-                                    obstacleslist.append(obstacles(pos_x,pos_y))
-                                    obstacle_x = 0
-                                    obstacle_y = 0
-                                    counter = 0 
-                                    first = True
-		                print " x : %.3f  y: %.3f  z: %.3f" %(p[0],p[1],p[2])
-                                obstacle_x += p[0]
-                                obstacle_y += p[1]
-                                counter += 1
+                              if (p[0]-pos_x)*(p[0]-pos_x)+(p[1]-pos_y)*(p[1]-pos_y)>0.4:
+                                  if(p[1]-pos_y)*(p[1]-pos_y)<0.1:
+                                      leftlist.append(obstacles(p[0],p[1]))
+                                      obstacle_x -= p[0]
+                                      obstacle_y -= p[1]
+                                      counter -= 1
+                                  else:
+                                      pos_x = obstacle_x/counter
+                                      pos_y = obstacle_y/counter
+                                      print " obstacle : ( %.3f , %.3f ) " %(pos_x , pos_y)
+                                      obstacle = ' ( %.3f , %.3f ) ,' %(pos_x , pos_y)
+                                      obstacleslist.append(obstacle)
+                                      obstacle_x = 0
+                                      obstacle_y = 0
+                                      counter = 0 
+                                      first = True
+                                      for check in leftlist:
+                                          if (check.x-p[0])*(check.x-p[0])+(check.y-p[1])*(check.y-p[1]) < 0.4 :
+                                              obstacle_x += check.x
+                                              obstacle_y += check.y
+                                              counter += 1
+                                              leftlist.remove(check)
+                                              pos_x = obstacle_x/counter
+                                              pos_y = obstacle_y/counter
+		              print " x : %.3f  y: %.3f  z: %.3f" %(p[0],p[1],p[2])
+                              obstacle_x += p[0]
+                              obstacle_y += p[1]
+                              counter += 1
         print " obstacle : ( %.3f , %.3f ) " %(pos_x , pos_y)                                     
-        obstacle = " ( %.3f , %.3f ) " %(pos_x , pos_y)
-        obstacleslist.append(obstacles(pos_x,pos_y))
-                           
-	rospy.loginfo(rospy.get_caller_id() + 'get cloud')
+        obstacle = ' ( %.3f , %.3f ) ' %(pos_x , pos_y)
+        obstacleslist.append(obstacle)
+        publish_final_pos(obstacleslist)                   
 
